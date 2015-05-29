c09d3c3e74f3615e3f5ad4bbc96e3d7452c5e640
Michael <mkappel1@swarthmore.edu>
2015-05-28 21:55:36 -0400

    Adds changes to allow for multiple slideshow instances on a single page

--- js/simple-slideshow.js
+++ js/simple-slideshow.js
@@ -1,10 +1,13 @@
 "use strict";
-var slides = document.getElementsByClassName("slide");
-var totalSlides = slides.length - 1;
-var activeSlide;
 
-function toggleSlides(direction) {
+function toggleSlides(btn, direction) {
+
+    var slideshowId = document.getElementById(btn.getAttribute('data-slideshow'));
+    var slides = slideshowId.getElementsByClassName("slide");
+    var totalSlides = slides.length - 1;
+    var activeSlide;
     var i;
+    
     //Loop through slides and identify the active slide
     for (i = 0; i < slides.length; i++) {
 
@@ -15,30 +18,22 @@ function toggleSlides(direction) {
     }
     //Determine which direction to slide
     if (direction === "left") {
-        slideLeft();
+        if (activeSlide === 0) {
+            slides[activeSlide].className = "slide dn";
+            slides[totalSlides].className = "slide active";
+        } else {
+            var nextSlideL = activeSlide - 1;
+            slides[activeSlide].className = "slide dn";
+            slides[nextSlideL].className = "slide active";
+        }
     } else if (direction === "right") {
-        slideRight();
-    }
-}
-
-function slideLeft() {
-    if (activeSlide === 0) {
-        slides[activeSlide].className = "slide dn";
-        slides[totalSlides].className = "slide active";
-    } else {
-        var nextSlideL = activeSlide - 1;
-        slides[activeSlide].className = "slide dn";
-        slides[nextSlideL].className = "slide active";
-    }
-}
-
-function slideRight() {
-    if (activeSlide === totalSlides) {
-        slides[activeSlide].className = "slide dn";
-        slides[0].className = "slide active";
-    } else {
-        var nextSlideL = activeSlide + 1;
-        slides[activeSlide].className = "slide dn";
-        slides[nextSlideL].className = "slide active";
+        if (activeSlide === totalSlides) {
+            slides[activeSlide].className = "slide dn";
+            slides[0].className = "slide active";
+        } else {
+            var nextSlideL = activeSlide + 1;
+            slides[activeSlide].className = "slide dn";
+            slides[nextSlideL].className = "slide active";
+        }
     }
 }
